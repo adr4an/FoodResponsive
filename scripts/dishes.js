8815f0a8dce90ff3f1002f30f6337f662b3c0528
adr4an <ad4antoh@gmail.com>
2024-02-14 01:39:20 +0800

    Cart Added

--- scripts/dishes.js
+++ scripts/dishes.js
@@ -1,4 +1,5 @@
 import { product } from "../data/product.js";
+import { cart } from "../data/cart.js";
 
 export function productDesign(){
     let html = '';
@@ -19,15 +20,43 @@ export function productDesign(){
                             <i class="fa-solid fa-star"></i>
                         </div>
                         <p id="prices"> Price : $${(item.price /100).toFixed(2)} </p>
-                        <button> Add to Bag </button>
+                        <button class="add-btn"
+                        data-product-id="${item.id}"> Add to Bag </button>
                     </div>
             </div>
         `;
 
     });
 
-    console.log(container);
-
     container.innerHTML = html;
+
+    const addBtn = document.querySelectorAll('.add-btn');
+    addBtn.forEach(item => {
+
+        item.addEventListener('click', () => {
+            const dataBtn = item.dataset;
+
+            let matchingId;
+                cart.forEach(cartItem => {
+                    if (cartItem.id === dataBtn) {
+                        matchingId = cartItem;
+                    }
+                });
+
+                if (matchingId) {
+                    matchingId.quantity += 1;
+                } else {
+                    cart.push({
+                        id: dataBtn,
+                        quantity: 1
+                    })
+                }
+
+                console.log(cart);
+
+
+        });
+        
+    });
 
 }
